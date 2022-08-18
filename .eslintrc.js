a9fa32b94ae09cb740b15b542da214a3b01c7322
aryanshridhar <aryanshridhar7@gmail.com>
2022-08-18 10:23:49 +0530

    website: few updates

--- .eslintrc.js
+++ .eslintrc.js
@@ -25,5 +25,6 @@ module.exports = {
         // Place to specify ESLint rules. Can be used to overwrite rules specified from the extended configs
         // e.g. "@typescript-eslint/explicit-function-return-type": "off",
         'prettier/prettier': 'error',
+        'react/no-unescaped-entities': 0,
     },
 };
--- src/Components/AboutSection.tsx
+++ src/Components/AboutSection.tsx
@@ -12,12 +12,13 @@ class AboutSection extends React.Component<props> {
             <div className="sectionContainer">
                 <div className="textContainer">
                     <p className="textStyle">
-                        Hey, I’m Aryan Shridhar, a third year undergraduate from Vellore Institute of Technology,
+                        Hey, I'm Aryan Shridhar, a fourth year undergraduate from Vellore Institute of Technology,
                         pursuing Computer Science and Engineering. I am an open source enthusiast and have a keen
                         interest in developing user-end applications. This curiosity brought me closer to frameworks
-                        like React.js, React-Native, Electron.js, Django, Flask and other similar frameworks including a
-                        few DL libraries like Pytorch, Keras and so. Apart from my computers, I love playing football
-                        and listening to music.
+                        like React.js, React-Native, Electron.js, Django, Flask and other similar frameworks. Currently,
+                        I am working with <a href="https://github.com/electron">Electron</a> as a GSoC'22 student
+                        enhancing <a href="https://github.com/electron/fiddle">fiddle's</a> architecture from a security
+                        standpoint.
                         <br />
                         <br /> Click <a href={this.props.resumeLink}>here</a> to view my resume :)
                     </p>
